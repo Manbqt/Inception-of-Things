608abf216370acc984a144462c00b08d278573ae
Man <bouquetmanon4@gmail.com>
2024-08-08 19:15:50 +0200

    With helm but only last service is running

--- P2/scripts/server.sh
+++ P2/scripts/server.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 apk update
+apk add helm
 
 # Kubectl installation
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
@@ -19,14 +20,33 @@ do
 done
 echo "k3s is installed"
 
+export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
+
 echo 'source <(kubectl completion bash)' >> ~/.bashrc
 echo 'complete -o default -F __start_kubectl k' >> ~/.bashrc
 echo "alias k='kubectl'" >> ~/.bashrc
 
 sleep 5
 
-for i in {1..3}
-do
-	kubectl apply -f /vagrant_shared/service_app$i.yaml --validate=false
-	kubectl apply -f /vagrant_shared/deployment_app$i.yaml --validate=false
-done
+helm create mychart
+chmod 666 /etc/rancher/k3s/k3s.yaml
+
+sleep 5
+
+cp -f /vagrant_shared/service.yaml mychart/templates/service.yaml
+cp -f /vagrant_shared/deployment.yaml mychart/templates/deployment.yaml
+cp -f /vagrant_shared/values.yaml mychart/values.yaml
+
+helm template my-release mychart/ -f mychart/values.yaml
+
+sleep 5
+
+
+helm install -f mychart/values.yaml test mychart/
+
+# for i in {1..3}
+# do
+# 	helm install -f P2/values.yaml test$i P2/
+# 	# kubectl apply -f /vagrant_shared/service_app$i.yaml --validate=false
+# 	# kubectl apply -f /vagrant_shared/deployment_app$i.yaml --validate=false
+# done
