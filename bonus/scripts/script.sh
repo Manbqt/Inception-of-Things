f5eb1c316eae92fdaa1ccf09756c4270fd1d2dd5
Man <bouquetmanon4@gmail.com>
2024-08-22 12:19:30 +0200

    Add docker, k3d and kubectl installation

--- bonus/scripts/script.sh
+++ bonus/scripts/script.sh
@@ -2,7 +2,40 @@
 
 cd $(dirname $0)
 
-install() {
+install_docker() {
+	sudo apt-get update
+	sudo apt-get install apt-transport-https ca-certificates curl gnupg2 software-properties-common
+	sudo curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+	sudo echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list
+	sudo apt-get update
+	sudo apt-get install docker-ce docker-ce-cli containerd.io
+	sudo systemctl enable docker
+
+	# Give permissions
+	sudo chown $USER /var/run/docker.sock
+}
+
+install_k3d() {
+	curl https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+}
+
+install_kubectl() {
+	sudo apt-get install -y apt-transport-https ca-certificates gnupg
+	if [ ! -f /etc/apt/keyrings ]
+	then
+		echo "file /etc/apt/keyrings is created"
+		touch /etc/apt/keyrings
+	fi
+
+	curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+	sudo chmod 644 /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+	echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+	sudo chmod 644 /etc/apt/sources.list.d/kubernetes.list
+	sudo apt-get update
+	sudo apt-get install -y kubectl
+}
+
+install_helm() {
 	curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
 	chmod 700 get_helm.sh
 	./get_helm.sh
@@ -32,6 +65,11 @@ install_argocd() {
 	kubectl patch configmap argocd-cmd-params-cm -n argocd -p '{"data": {"server.insecure": "true"}}'
 }
 
+wait_argocd() {
+	# Wait for argocd-server to be ready
+	kubectl -n argocd wait --for=condition=available --timeout=600s deployment/argocd-server
+}
+
 argocd() {
 	kubectl apply -n argocd --wait -f ../config/appproject.yaml
 	kubectl apply -n argocd --wait -f ../config/application.yaml
@@ -40,10 +78,6 @@ argocd() {
 	echo "Username: \`admin\` Password: \`$passwd\`"
 }
 
-wait_argocd() {
-	# Wait for argocd-server to be ready
-	kubectl -n argocd wait --for=condition=available --timeout=600s deployment/argocd-server
-}
 
 dev() {
 	kubectl create namespace dev
@@ -65,12 +99,14 @@ gitlab() {
 
 
 	echo "The password of gitlab root is \`$(kubectl get secret gitlab-gitlab-initial-root-password -n gitlab -ojsonpath='{.data.password}' | base64 --decode)\`"
-
 }
 
 if [ $# -eq 0 ]
 then
-	install
+	install_docker
+	install_k3d
+	install_kubectl
+	install_helm
 	install_argocd
 	wait_argocd
 	argocd
